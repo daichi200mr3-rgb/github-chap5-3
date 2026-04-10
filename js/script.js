cf3ad3c0a3cc5689c0e6bada6dffddb25a30cbae
masao doi <166479026+ddd-md@users.noreply.github.com>
2026-04-10 18:19:21 +0900

    Initial Commit develop

---
+++ js/script.js
@@ -0,0 +1,4 @@
+function showIntroduction() {
+    const introduction = "こんにちは、私はJohn Smithです。Web開発者で、プログラミングが大好きです。このサイトでは自己紹介を共有します。趣味は旅行、音楽、スポーツです。";
+    document.getElementById("intro").textContent = introduction;
+}
